d4c21db6dd38052abe438c0162a9469f830fb15f
sagar0salunke <sagarsalunkeno.1@gmail.com>
2024-08-08 06:20:00 +0530

    client login regi

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,24 +2,27 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AccountService } from './account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,NavComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  http = inject(HttpClient);
-  title = 'DatingApp';
-  users: any;
+  private accountService = inject(AccountService);
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.setCurrentUser();
   }
-}
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }}
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { AccountService } from '../account.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  accountService = inject(AccountService);
+  model: any = {};
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
